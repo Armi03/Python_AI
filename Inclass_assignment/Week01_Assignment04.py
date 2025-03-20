7ec21db297ccd2cc5c232b00c23f52ad3ccfa0a8
Armi Amipara <Armi.Amipara@metropolia.fi>
2025-03-20 14:54:13 +0200

    Week01 Assignment04

---
+++ Inclass_assignment/Week01_Assignment04.py
@@ -0,0 +1,10 @@
+def calculate_grade():
+    try:
+        score = float(input("Enter the student's score: "))
+        grade = 'A' if score >= 90 else 'B' if score >= 80 else 'C' if score >= 70 else 'D' if score >= 60 else 'E'
+        print(f"The student's grade is: {grade}")
+    except ValueError:
+        print("Invalid input. Please enter a numeric score.")
+
+if __name__ == "__main__":
+    calculate_grade()
